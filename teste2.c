089d823fb153abc0f2c276ee6ad93b4205e3d428
gabrielahf <127217373+gabrielahf@users.noreply.github.com>
2023-03-23 10:56:16 -0300

    Update teste2.c

--- teste2.c
+++ teste2.c
@@ -5,11 +5,13 @@ int main() {
     printf ("informe um numero:");
     scanf("%d", &valor);
 
-//verifica se valor eh par
+//verifica se o valor eh par
     if(valor % 2 == 0) {
     printf("valor eh par!\n'");
     } else {
+ // cria um comando de aviso caso o numero nao seja par
         printf("valor eh impar!\n'");
+ // sinaliza condicionais se o valor eh multiplo por 3,5,7
         if(valor % 3 == 0 || valor % 5 == 0 || valor % 7 == 0 ) {
     printf("valor multiplo de 3, 5 OU 7!\n'");
 

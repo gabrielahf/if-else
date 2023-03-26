574f740ff1806fa455f79f3e4e2f9f25c71d1f7d
gabrielahf <127217373+gabrielahf@users.noreply.github.com>
2023-03-26 16:54:30 -0300

    Add files via upload

---
+++ teste4.c
@@ -0,0 +1,26 @@
+#include "stdio.h"
+#include "math.h"
+int main() {
+
+    float la, lb, lc;
+    float sp; 
+    float area;
+
+    printf("lado a:  ");
+    scanf("%f", &la); 
+    printf("lado b:  ");
+    scanf("%f", &lb);
+    printf("lado c:  ");
+    scanf("%f", &lc);
+
+    if ( lc >= (la + lb) || la >= (lb + lc) || lb >= (la + lc)); {
+       printf("valor invalido\n");
+    } else {
+            sp = (la + lb + lc) / 2;
+            area = sqrt (sp * (sp - la) * (sp - lb) * (sp - lc));
+                          printf("area:  ");
+                          scanf("%f", &area);
+    } 
+    
+
+}
